cbe1f41c636b14045b731cefdebeab550df9cd23
BartoszKwec <bartosz.kwec@softwarehut.com>
2025-11-06 20:34:04 +0100

    Initial project setup: Spring Boot 3.5.7 + Spring Cloud 2025.0.0 + WebFlux Gateway

--- src/main/java/pl/kwec/mymanagerapigateway/MymanagerApiGatewayApplication.java
+++ src/main/java/pl/kwec/mymanagerapigateway/MymanagerApiGatewayApplication.java
@@ -2,8 +2,10 @@ package pl.kwec.mymanagerapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MymanagerApiGatewayApplication {
 
     public static void main(String[] args) {
