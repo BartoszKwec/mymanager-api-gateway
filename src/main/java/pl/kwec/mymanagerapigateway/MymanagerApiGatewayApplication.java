2e8388728549d5da0539c78cac3b77a7764bf7a5
BartoszKwec <bartosz.kwec@softwarehut.com>
2025-11-06 20:12:35 +0100

    Initial commit - empty gateway project

---
+++ src/main/java/pl/kwec/mymanagerapigateway/MymanagerApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package pl.kwec.mymanagerapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymanagerApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymanagerApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kwec/mymanagerapigateway/MymanagerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kwec.mymanagerapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymanagerApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
